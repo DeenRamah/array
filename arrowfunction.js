fc4df4ec092fb290fbcb89d14dde02dedfdb7e77
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-25 12:18:28 +0300

    Create arrowfunction.js

---
+++ arrowfunction.js
@@ -0,0 +1,10 @@
+//use arrow function(=>) for consise function definitions, especiallt for callbaccks and anonymous function
+//Regualr function
+function add(a,b){
+  return a + b;
+}
+
+//Arrow functions
+const add = (a,b)=> a +b;
+//usage
+console.log(add(2,3));//output:5
