1ebfe856b3c5b72e20649a002fc13b1dfb181459
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-25 15:38:03 +0300

    Create destructuring.js

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+//object destructuring
+const person = {name: 'John', age:25 };
+const {name, age} = person;
+console.log(name);
+console.log(age);
+
+//Array destructuring
+const numbers =[1,2,3];
+const [first, second, third] = numbers;
+
+console.log(first);
+console.log(second);
+console.log(third);
